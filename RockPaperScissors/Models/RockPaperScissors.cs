cbde048a75d0392f69a0e08272a2c28c5840089e
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-15 13:31:13 -0700

    add code for paper v scissors
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- RockPaperScissors/Models/RockPaperScissors.cs
+++ RockPaperScissors/Models/RockPaperScissors.cs
@@ -12,7 +12,7 @@ namespace RockPaperScissors.Models
       {
         return "Player 1 Win";
       }
-      else if (player1 == "rock" && player2 == "paper" || player1 == "scissors" && player2 == "rock")
+      else if (player1 == "rock" && player2 == "paper" || player1 == "scissors" && player2 == "rock" || player1 == "paper" && player2 == "scissors")
       {
         return "Player 2 Win";
       }
