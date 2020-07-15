5a1898d403590325447ac6964cfc9a0b8e2bb78d
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-15 13:24:28 -0700

    add code for scissors v rock
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- RockPaperScissors/Models/RockPaperScissors.cs
+++ RockPaperScissors/Models/RockPaperScissors.cs
@@ -12,10 +12,11 @@ namespace RockPaperScissors.Models
       {
         return "Player 1 Win";
       }
-      else if (player1 == "rock" && player2 == "paper")
+      else if (player1 == "rock" && player2 == "paper" || player1 == "scissors" && player2 == "rock")
       {
         return "Player 2 Win";
       }
+      
 
       return "dummy data so compiler doesn't complain";
     }
