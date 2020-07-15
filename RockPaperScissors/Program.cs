df9b1506725d4e472bd2de5d90b20b0b1bbd3935
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-15 13:34:00 -0700

    add basic ui
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,10 +1,18 @@
+using System;
+using RockPaperScissors.Models;
+
 namespace RockPaperScissors.Program
 {
   public class RPSProgram
   {
     public static void Main()
     {
-
+      Console.WriteLine("Let's play Rock, Paper, Scissors!");
+      Console.WriteLine("Player 1, make your pick! [type rock, paper, or scissors]");
+      string player1 = Console.ReadLine();
+      Console.WriteLine("Player 2, make your pick! [type rock, paper, or scissors]");
+      string player2 = Console.ReadLine();
+      Console.WriteLine(Game.DetectWinner(player1, player2));
     }
   }
 }
